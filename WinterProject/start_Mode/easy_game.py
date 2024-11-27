3f944e6a7b20b02f64395558586f64594b05ccd7
treboL-uismi <luismiguelgarciadesousa96@gmail.com>
2024-11-27 11:29:22 +0100

    feat: :sparkles: Added Images in the cards

--- WinterProject/start_Mode/easy_game.py
+++ WinterProject/start_Mode/easy_game.py
@@ -1,12 +1,33 @@
 import reflex as rx
-
 from rxconfig import config
+
+from PIL import Image
+
+img = Image.open(r"assets\personajes_qsq_02.png")
+
+
+card = [(0, 0, 510, 771),
+        (510, 0, 1020, 771),
+        (1020, 0, 1530, 771),
+        (1530, 0, 2040, 771),
+        (0, 771, 510, 1542),
+        (510, 771, 1530, 1542),
+        (1020, 771, 1530, 1542),
+        (1530, 771, 2040, 1542)]
+
 
 def game_board():
     return rx.grid(
         rx.foreach(
             rx.Var.range(8),
-            lambda i: rx.card(f"Card {i + 1}", height="10vh"),
+            lambda i: rx.card(rx.inset(rx.image(src=img.crop(card[0]),
+                         width="100%",
+                         height="auto",
+                         ),
+                        side="center",
+                        pb="current",
+            ),          
+          ),
         ),
         columns="4",
         spacing="4",
