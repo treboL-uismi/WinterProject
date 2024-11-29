6629b1be52c47cca588fe9f51f1e2906ef42886e
treboL-uismi <luismiguelgarciadesousa96@gmail.com>
2024-11-29 14:00:48 +0100

    refactor: :rocket: Trying to get the input of the chat

--- back/src/main.py
+++ back/src/main.py
@@ -1,4 +1,8 @@
 import random
+import reflex as rx
+
+from WinterProject.start_Mode.easy_game import action_bar as easy_action
+from WinterProject.start_Mode.hard_game import action_bar as hard_action
 
 def randChr(chrDict: dict[str, list[str]]) -> str:
     chr = random.choice(list(chrDict.items()))
@@ -6,3 +10,10 @@ def randChr(chrDict: dict[str, list[str]]) -> str:
     #caractList = chr[1]
     
     return nombre
+
+def getInputEasy():
+    ...
+
+def getInputHard():
+    ...
+    
